721a2c1fb577d8d0cb59fcb27b8b1f075e96a2f5
Sara Thampi <saraannthampi@gmail.com>
2024-01-29 12:46:50 +0000

    text updated

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,21 +8,24 @@ export default function Info() {
     return(
         <div className="info-wrapper"> 
             <p>
-                Hi, My name is Sara Thampi and I have just completed an intensive coding bootcamp, learning <strong>JavaScript, React</strong> and <strong>Node.js.</strong>
+                Hi, My name is Sara Thampi and I have just completed an intensive coding bootcamp with School of Code, learning <strong>JavaScript, React</strong> and <strong>Node.js.</strong>
             </p>
             <p>
-                When I was a child my dad bought an Amstrad computer for his accountancy job.  I discovered a guide for the programming language GW-Basic among his computer manuals and taught myself how to make little programs. This led me to take a GCSE in IT: We made pages in Teletext, little programs in BASIC and were the first people in school to have an email address. But our teacher warned us, that a Computer Science A-level was much harder and we needed much more knowledge than he had taught us.
+                When I was a child my dad bought an Amstrad computer for his accountancy job.  I discovered a guide for the programming language GW-Basic among his computer manuals and taught myself how to make little programs. This led me to take a GCSE in IT: We made pages in Teletext, little programs in BASIC and were the first people in school to have an email address. I wanted to study further, but our teacher told us we needed much more knowledge to study a Computer Science A-level. 
             </p>
             <p>
-                So I studied Maths, Physics and German instead. I was not talented at talking and I wanted to challenge myself: I decided to study a languages degree (German and Danish) at Edinburgh University; I loved the written work, but found the oral work challenging.
-            </p>
-
-            <p>
-                I realised I needed to do something to change this, so I applied for jobs in retail. This helped slightly, but my progress was not as good as I hoped. It was only when I referred myself to counselling for social anxiety, my life started to change.
+                So I studied Maths, Physics and German instead. I was fascinated with different cultures and ways of seeing the world, so I decided to study a languages degree (German and Danish) at Edinburgh University; I loved the written work, but found the oral work challenging.
             </p>
 
             <p>
-                I left retail and became a homeless shelter support worker. I helped the residents apply for benefits and housing and get health support. I realised many of them struggled with computers and really needed my help. This reminded me of how much I wanted to work in tech. So I googled how I could train as a computer programmer. I was one of 90 people selected for the School of Code bootcamp from over 1000 applicants. I totally loved learning to code in teams and haven&apos;t looked back since.
+                I realised I needed to focus on developing communication skills, so I applied for jobs in retail. I enjoyed working in teams, and getting to know about our customers&apos; lives. Just before lockdown, I referred myself to counselling for social anxiety, and my confidence started improving significantly.
+            </p>
+
+            <p>
+                I left retail and became a homeless shelter support worker. I helped the residents apply for benefits and housing and get health support. I realised many of them struggled with computers and really needed my help. I came to the sad realisation, that without help, many of them are denied access to the support available to them. This reminded me of how much I wanted to work in tech. So I googled how I could train as a computer programmer. I was one of 90 people selected for the School of Code bootcamp from over 1000 applicants. I totally loved learning to code in teams and haven&apos;t looked back since.
+            </p>
+            <p>
+                I loved working in teams and collaborating with different people with varied perspectives. I would love to create tech that is accessible to all people, not just those who are technically literate. I think it is important that everyone should be able to use technology and gain its benefits.
             </p>
 
             <p>
