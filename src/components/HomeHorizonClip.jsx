0c5218c1a74ccfb4f18b73da0d05d819edc21b4e
Sara Thampi <saraannthampi@gmail.com>
2024-01-30 19:51:17 +0000

    vid spacing 4

--- src/components/HomeHorizonClip.jsx
+++ src/components/HomeHorizonClip.jsx
@@ -5,16 +5,19 @@ import React from "react";
  class HomeHorizonClip extends React.Component {
     render() {
       const options = {
-        height: '390',
-        width: '640',
+        height: '100%',
+        width: '100%',
         playerVars: {
           autoplay: 1,
           controls: 1,
         },
       };
   
-      return <div className="vid-container"> <YouTube videoId="QxMD300peCA" options={options} onReady={this._onReady} id="video" className="video-frame" /> </div>;
-    }
+      return (
+          <div className="vid-container"> 
+            <YouTube videoId="QxMD300peCA" options={options} onReady={this._onReady} id="video" className="video-frame" /> 
+          </div>
+    )}
   
     _onReady(event) {
       event.target.pauseVideo();
