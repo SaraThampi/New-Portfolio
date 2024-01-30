fe0f12b59796071a1f9db5f0d3c7d913d1a4627b
Sara Thampi <saraannthampi@gmail.com>
2024-01-30 13:54:30 +0000

    vid description spacing changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,10 @@ export default function About() {
             <h2>Projects</h2>
             <h3>Home Horizon - Homeless Shelter App</h3>
             <p className="vid-description">
-                In a team of six, we collaborated on a coding project for 4 weeks. I work in a homeless shelter and my teammates wanted to do something meaningful and asked me how they could help. I explained my frustrations. Even though we do some amazing work with limited resources, we could be so much more efficient. I told them about the challenges of sharing data about the service users and how it leads to work being repeated or not being done at all. We all agreed this was a problem we needed to tackle. We surveyed homeless support workers from three different organisations and developed an app to help with their problems. We tried to make the app as simple to use as possible: Several of the people we surveyed, struggled with tech. Some of them even needed help to fill out our survey on Google Forms. We even had a session with a homeless shelter manager (not my manager!), who tried our app for himself and gave us encouraging feedback.
+                In a team of six, we collaborated on a coding project for 4 weeks. I work in a homeless shelter. My teammates wanted to do something meaningful and asked me how they could help. I explained my frustrations: Even though we do some amazing work with limited resources, we could be so much more efficient. My colleagues and I have experienced many challenges with sharing data about the service users. This leads to work being repeated or not being done at all.
+            </p>
+            <p className="vid-description">    
+                We all agreed this was a problem we needed to tackle. We surveyed homeless support workers from three different organisations and developed an app to help with their problems. We tried to make the app as simple to use as possible: Several of the people we surveyed, struggled with tech. Some of them even needed help to fill out our survey on Google Forms. We even had a session with a homeless shelter manager (not my manager!), who tried our app for himself and gave us encouraging feedback.
             </p>
             <p className="vid-description">
                 Please watch the video below to see a demonstration of our app:
